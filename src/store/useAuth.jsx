fb6ff746b8ec88d7d05c261074a60e79913c6562
abdinazarov7799 <parolxato@gmail.com>
2023-11-13 16:49:11 +0500

    .

--- src/store/useAuth.jsx
+++ src/store/useAuth.jsx
@@ -2,13 +2,12 @@ import {create} from "zustand";
 import {devtools, persist} from "zustand/middleware";
 import {useSettingsStore} from "./settingsStore.jsx";
 import {get} from "lodash";
-const setIsLogin = useSettingsStore(state => get(state, 'setIsLogin', () => {}));
+
 let authStore = (set) => ({
     users: [],
     addUser: (newUser) => set((state) => ({ users: [...state.users, newUser] })),
     logOut: () => {
         set(() => ({ users: null }));
-        setIsLogin(false);
     }
 })
 
